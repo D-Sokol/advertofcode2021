9173f5088f797f304419c2313db223b60c2e26c2
Dmitrii Sokol <sokol.dmitry.f@gmail.com>
2021-12-04 21:42:26 +0300

    Allowed relpacement of input by small example via variable in template.

--- Template.py
+++ Template.py
@@ -3,10 +3,15 @@ import fileinput
 
 # If True, this script solves the first part of the puzzle, the second one otherwise.
 EASY = True
+# If True, example from the puzzle condition is used as input.
+TEST = False
 DAY = 1
 
+INPUT_FILE = f'{DAY}{"t" if TEST else ""}.txt'
+
+
 data = []
-for line in fileinput.input(f'{DAY}.txt'):
+for line in fileinput.input(INPUT_FILE):
     line = line.strip()
     # line = int(line)
     data.append(line)
